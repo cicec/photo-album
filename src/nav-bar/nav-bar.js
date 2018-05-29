b3197ec9ab2fe5c5baa6db888199acaf29967ddf
clancysong <clancysong@gmail.com>
2018-05-29 19:36:39 +0800

    add nav-bar component

---
+++ src/nav-bar/nav-bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <aside>
+                nav bar
+            </aside>
+        )
+    }
+}
+
+export default NavBar
