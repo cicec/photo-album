377c97dea6d43dbe46d0b4a9e4f5f361b789a0de
clancysong <clancysong@gmail.com>
2018-08-01 15:35:55 +0800

    Reorganize the project catalog.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import './init.css'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
