18d2eb824dcb476aa867bbe30c7a421879749830
clancysong <clancysong@gmail.com>
2018-10-30 15:56:24 +0800

    使用 popup-card 包装 user-card 组件

--- src/main/user-card/user-card.js
+++ src/main/user-card/user-card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
+import popupCard from '../popup-card'
 import Toast from '../../components/toast'
 import './user-card.css'
 
@@ -11,7 +12,6 @@ class UserCard extends Component {
         this.state = { isChanged: false, userInfo: {} }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-        this.closeCard = this.closeCard.bind(this)
     }
 
     handleChange(event) {
@@ -46,47 +46,35 @@ class UserCard extends Component {
         })
     }
 
-    closeCard() {
-        const { stores: { uiState } } = this.props
-        uiState.changeCurrentState(uiState.states.DEFAULT)
-    }
-
     render() {
         const { isChanged } = this.state
         const { stores: { userStore: { user: { avatar, name, description, phone, email } } } } = this.props
         return (
             <div className="user-card">
-                <div className="card">
-                    <button type="button" className="close" onClick={this.closeCard}>
-                        <svg className="icon" aria-hidden="true">
-                            <use xlinkHref="#icon-close" />
-                        </svg>
-                    </button>
-                    <label htmlFor="avatar-input" className="avatar-label">
-                        <div className="avatar">
-                            <img src={avatar} alt="" ref={(e) => { this.avatarImage = e }} />
-                        </div>
-                    </label>
-                    <input type="file" accept="image/*" name="avatar" id="avatar-input" className="avatar-input" onChange={this.handleChange} />
-                    <input type="text" className="name" name="name" defaultValue={name} onChange={this.handleChange} />
-                    <textarea rows="3" maxLength="40" type="text" className="description" name="description" defaultValue={description} onChange={this.handleChange} onKeyDown={this.checkEnter} />
-                    <label htmlFor="phone" className="phone-label">
-                        <svg className="icon" aria-hidden="true">
-                            <use xlinkHref="#icon-mobile" />
-                        </svg>
-                        <input type="text" className="phone" name="phone" defaultValue={phone} onChange={this.handleChange} />
-                    </label>
-                    <label htmlFor="email" className="email-label">
-                        <svg className="icon" aria-hidden="true">
-                            <use xlinkHref="#icon-mail" />
-                        </svg>
-                        <input type="text" className="email" name="email" defaultValue={email} onChange={this.handleChange} />
-                    </label>
-                    {isChanged ? <button type="button" className="save" onClick={this.handleSubmit}>保存</button> : ''}
-                </div>
+                <label htmlFor="avatar-input" className="avatar-label">
+                    <div className="avatar">
+                        <img src={avatar} alt="" ref={(e) => { this.avatarImage = e }} />
+                    </div>
+                </label>
+                <input type="file" accept="image/*" name="avatar" id="avatar-input" className="avatar-input" onChange={this.handleChange} />
+                <input type="text" className="name" name="name" defaultValue={name} onChange={this.handleChange} />
+                <textarea rows="3" maxLength="40" type="text" className="description" name="description" defaultValue={description} onChange={this.handleChange} onKeyDown={this.checkEnter} />
+                <label htmlFor="phone" className="phone-label">
+                    <svg className="icon" aria-hidden="true">
+                        <use xlinkHref="#icon-mobile" />
+                    </svg>
+                    <input type="text" className="phone" name="phone" defaultValue={phone} onChange={this.handleChange} />
+                </label>
+                <label htmlFor="email" className="email-label">
+                    <svg className="icon" aria-hidden="true">
+                        <use xlinkHref="#icon-mail" />
+                    </svg>
+                    <input type="text" className="email" name="email" defaultValue={email} onChange={this.handleChange} />
+                </label>
+                {isChanged ? <button type="button" className="save" onClick={this.handleSubmit}>保存</button> : ''}
             </div>
         )
     }
 }
 
-export default UserCard
+export default popupCard(UserCard)
